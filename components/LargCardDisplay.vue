b8fe38f1420f00c74850e74476a44a871eb39a56
manik <mdmanik081498@gmail.com>
2023-03-30 17:47:19 +0600

    fire Extinguisher

---
+++ components/LargCardDisplay.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div class="container" v-for="cardInfo in largCardInfo" :key="cardInfo.id">
+        <h4 class="header">
+            {{ cardInfo.title }}
+        </h4>
+        <p class="snippet" >
+            {{ cardInfo.snippet }}
+        </p>
+        <div class="cards-container">
+             <LargCard 
+                v-for="card in cardInfo.cards"
+                :key="card.id"
+                :card="card"
+            />
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+import  largeCardSections from '../store/data';
+import LargCard from '../components/LargCard.vue'
+
+export default {
+ data() {
+   return {
+      largCardInfo: largeCardSections
+   }
+ },
+ components: {
+    LargCard
+ }
+}
+</script>
+
+<style scoped>
+.container {
+    margin-top: 2rem;
+}
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+    .snippet {
+        color: grey;
+        margin-bottom: 1.5rem;
+    }
+    .cards-container {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+      <Navber></Navber>
+      <br> <br>
+       <LargCardDisplay></LargCardDisplay>
+  
+
+     
+  </div>
+  
+</template>
+
+ <script>
+ import  largeCardSections from '../components/LargCardDisplay.vue';
+
+export default {
+  components: {
+    largeCardSections
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
